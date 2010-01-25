56476948a4920918bebf79e8823cc1454f9fa29d
Amarok <Amarok@localhost>
2010-01-25 22:48:57 +0100

    (no commit message)

--- src/test.py
+++ src/test.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys, random
 
 pygame.init()
 
@@ -7,14 +7,22 @@ class EventGenerator:
         pass
 
     def run(self):
-        pygame.event.post(pygame.event.Event(42, {"test number" : 10}))
+        if random.random() > 0.999:
+            print "creating and sending event"
+            pygame.event.post(pygame.event.Event(42, {"test number" : 10}))
 
 
+size = 200, 200
+screen = pygame.display.set_mode(size)
 
 eg = EventGenerator()
 
 while 1:
     for event in pygame.event.get():
         print event.type, pygame.event.event_name(event.type)
-        eg.run()
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                    print "exiting..."
+                    sys.exit()
+    eg.run()
+    pygame.display.flip()
 
